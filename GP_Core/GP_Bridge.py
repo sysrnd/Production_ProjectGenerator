bd2cd5ca25b0ecf184e2da711c8db2ce4e3caed9
arturoalcibia <arturoalcibia@hotmail.com>
2018-03-25 21:43:42 -0600

    connected comboBox to signal

--- GP_Core/GP_Bridge.py
+++ GP_Core/GP_Bridge.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+from PyQt4 import QtCore, QtGui
+
+
 class GP_Bridge(object):
 	def __init__(self, window):
+		
 		self.window = window
+		QtCore.QObject.connect(self.window.cmB_type, QtCore.SIGNAL("currentIndexChanged(QString)"), self.populateUI)
+
+		self.UIDict = {0: self.mexicartoon, 1: self.spot, 2: self.marca, 3: self.campana}
+
+	def populateUI(self):
+		functionToCall = self.UIDict[self.window.cmB_type.currentIndex()]()
+		#functionToCall()
+		
+
+	def mexicartoon(self):
+
+		print 'test'
+
+	def spot(self):
+
+		print 'test'
+
+	def marca(self):
+		pass
+
+	def campana(self):
+		pass
--- GP_app.py
+++ GP_app.py
@@ -1,7 +1,4 @@
-"""
-StandAlone pyqt4
-
-"""
+# -*- coding: utf-8 -*-
 import sys
 import os
 from PyQt4 import QtCore, QtGui
